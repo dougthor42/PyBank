174732c8b69d600f7b33c6ec7e816755be724f8c
Douglas Thor <dougthor42@users.noreply.github.com>
2015-06-23 17:46:42 -0700

    Refactored __init__.py, added docstring and notes.

--- pybank/__init__.py
+++ pybank/__init__.py
@@ -111,80 +111,126 @@ Transaction matching and download start date = previous download date.
         transaction_00? Or even transaction_000?
 # TODO: Does Google Wallet have OFX?
 """
-import logging
-from logging.handlers import TimedRotatingFileHandler as TRFHandler
+### Imports #################################################################
 import datetime
 import os.path
+import logging
+from logging.handlers import TimedRotatingFileHandler as TRFHandler
 
-
+### Constants ###############################################################
 __version__ = "0.0.2.1"
 __project_url__ = "https://github.com/dougthor42/PyBank"
 __project_name__ = "PyBank"
 
+LOG_LEVEL_FILE = logging.DEBUG
+LOG_LEVEL_CONSOLE = logging.DEBUG
 
-__all__ = [__version__,
-           __project_name__,
-           __project_url__,
-           ]
 
-# Set up logging. DEBUG to console, INFO to file.
-# see https://aykutakin.wordpress.com/2013/08/06/logging-to-console-and-file-in-python/
-# Logging format includes milliseconds.
-# See http://stackoverflow.com/a/7517430/1354930
-logfmt = ("%(asctime)s.%(msecs)03d"     # Note implicit string concatenation.
-          " [%(levelname)-8.8s]"
-          " [%(module)-8.8s]"
-          " [%(funcName)-10.10s]"
-          "  %(message)s"
-          )
-datefmt = "%Y-%m-%d %H:%M:%S"
+def _setup_logging():
+    """
+    Set up logging for the entire package.
 
-# Create the logger
-logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
+    Log strings are sent to both the console and a log file.
 
-# Create console handler and set level
-handler = logging.StreamHandler()
-handler.setLevel(logging.DEBUG)
-formatter = logging.Formatter(logfmt, datefmt)
-handler.setFormatter(formatter)
-logger.addHandler(handler)
+    The file is a TimedRotatingFileHandler set up to create a new log file
+    every Sunday at midnight. This should keep the log files small-ish
+    while also keeping the number of log files down. All log files are
+    kept (none are automatically deleted by backupCount parameter).
 
-# Create file handler and set level
-# Use a Rotating file handler so that each week has a different file.
-# see https://docs.python.org/3.4/library/logging.handlers.html#logging.handlers.TimedRotatingFileHandler
-# and http://www.blog.pythonlibrary.org/2014/02/11/python-how-to-create-rotating-logs/
+    Log lines look like so::
 
-# Build the logfile path.
-dirname = os.path.dirname(os.path.abspath(__file__))
-rootpath = os.path.split(dirname)[0]
-logpath = os.path.join(rootpath, "log")
-logfile = os.path.join(logpath, "PyBank.log")
+        2015-06-23 17:04:10.409 [DEBUG   ] [gui     ] [_color_dol]  message...
+        |--------| |----------| |--------| |--------| |----------|  |--------)
+             ^          ^            ^          ^            ^          ^
+             |          |            |          |            |          |
+        Date +          |            |          |            |          |
+        Time -----------+            |          |            |          |
+        Level Name (8 char) ---------+          |            |          |
+        Module Name (8 char) -------------------+            |          |
+        Function Name (10 char) -----------------------------+          |
+        Message --------------------------------------------------------+
 
-rollover_time = datetime.time.min       # midnight
-try:
-    handler = TRFHandler(logfile,
-                         when="W6",         # Sunday night
-#                         interval=7,
-#                         backupCount=5,
-                         atTime=rollover_time,
-#                         delay=True,
-                         )
-except FileNotFoundError:
-    # we probably need to go up one more level to find the log folder
-    rootpath = os.path.split(rootpath)[0]
+    All dates and times are ISO 8601 format, local time.
+
+
+    Parameters:
+    -----------
+    None
+
+    Returns:
+    --------
+    None
+
+    Notes:
+    ------
+
+    1. Since I cannot get ms in the date format, I use the logger `msecs`
+       attribute in the log format string.
+    2. cx_freeze puts .py files in library.zip, so this means that I have
+       to go up one additional level in the directory tree to find the
+       `log` folder.
+
+    References:
+    -----------
+    Logging different levels to different places:
+        https://aykutakin.wordpress.com/2013/08/06/logging-to-console-and-file-in-python/
+
+    Adding milliseconds to log string:
+        http://stackoverflow.com/a/7517430/1354930
+
+    TimedRotatingFileHandler:
+        https://docs.python.org/3.4/library/logging.handlers.html#logging.handlers.TimedRotatingFileHandler
+
+    TimedRotatingFileHandler:
+        http://www.blog.pythonlibrary.org/2014/02/11/python-how-to-create-rotating-logs/
+    """
+    logfmt = ("%(asctime)s.%(msecs)03d"
+              " [%(levelname)-8.8s]"
+              " [%(module)-8.8s]"       # Note implicit string concatenation.
+              " [%(funcName)-10.10s]"
+              "  %(message)s"
+              )
+    datefmt = "%Y-%m-%d %H:%M:%S"
+
+    # Create the logger
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+
+    ### Console Handler #####################################################
+    handler = logging.StreamHandler()
+    handler.setLevel(LOG_LEVEL_CONSOLE)
+    formatter = logging.Formatter(logfmt, datefmt)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+
+    ### File Handler ########################################################
+    # Build the logfile path.
+    dirname = os.path.dirname(os.path.abspath(__file__))
+    rootpath = os.path.split(dirname)[0]
     logpath = os.path.join(rootpath, "log")
     logfile = os.path.join(logpath, "PyBank.log")
+
+    # see Note #2
+    if not os.path.isdir(logpath):
+        rootpath = os.path.split(rootpath)[0]       # up one level
+        logpath = os.path.join(rootpath, "log")
+
+    logfile = os.path.join(logpath, "PyBank.log")
+
+    rollover_time = datetime.time.min       # midnight
     handler = TRFHandler(logfile,
-                         when="W6",         # Sunday night
-#                         interval=7,
-#                         backupCount=5,
+                         when="W6",         # Sunday
+                         #interval=7,       # when=Sunday -> not needed
+                         #backupCount=5,
                          atTime=rollover_time,
-#                         delay=True,
+                         #delay=True,
                          )
+    handler.setLevel(LOG_LEVEL_FILE)
+    formatter = logging.Formatter(logfmt, datefmt)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
 
 
-handler.setLevel(logging.DEBUG)
-formatter = logging.Formatter(logfmt, datefmt)
-handler.setFormatter(formatter)
-logger.addHandler(handler)
+### Moudle Executions #######################################################
+_setup_logging()
