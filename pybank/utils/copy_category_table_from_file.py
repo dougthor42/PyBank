6c3ac93e2cb456bad24f0dadfa0c8b9fa1481fbc
Douglas Thor <doug.thor@gmail.com>
2015-12-08 00:24:34 -0800

    got categories.txt -> database working. Still need to refactor and comment.

--- pybank/utils/copy_category_table_from_file.py
+++ pybank/utils/copy_category_table_from_file.py
@@ -21,14 +21,23 @@ import sqlite3
         # 2. if so, split out only the very last element.
         # 3. check if th
 
-def something(item, delim='.', retval=None):
-    """ """
+def recurse(items, delim='.', retval=None):
+    """
+    recuse through all of the elements, finding parents.
+
+    if parent = 1, stop
+
+    then,
+    """
+    if retval is None:
+        retval = []
+
     # try and split the item
     split_item = item.split(delim)
     if len(split_item) == 1:
         print("adding {}.".format(item))
     else:
-        something(delim.join(split_item[:-1]), delim)
+        recurse(delim.join(split_item[:-1]), delim)
 
 
 def thing(items, delim):
@@ -37,7 +46,6 @@ def thing(items, delim):
     for item in items:
         # does the item have a parent?
         item = item.strip()
-        print(item)
         split = item.split(delim)
         if len(split) == 1:
             # no, it does not have a parent
@@ -45,8 +53,9 @@ def thing(items, delim):
         else:
             # yes, it does have a parent
             # look up that parent in the categories list
-            parent = categories[split[-2]][0]
-            categories[split[-1]] = (index, parent)
+            parent_item = delim.join(split[:-1])
+            parent = categories[parent_item][0]
+            categories[item] = (index, parent)
         index += 1
     return categories
 
@@ -56,12 +65,16 @@ def sort_categories(categories):
     return sorted([(i, k, parent) for (k, (i, parent)) in categories.items()])
 
 
-def write_to_db(items):
+def write_to_db(items, delim):
     conn = sqlite3.connect("../../blank_db_test.sqlite")
     cursor = conn.cursor()
 
     for item in items:
+        item = list(item)
+        item[1] = item[1].split(delim)[-1]
+        item = tuple(item)
         sql = "INSERT INTO category VALUES{};".format(item)
+        print(sql)
         cursor.execute(sql)
 
     conn.commit()
@@ -75,7 +88,31 @@ if __name__ == "__main__":
     a = thing(data, ":")
     b = sorted([(i, k, parent) for (k, (i, parent)) in a.items()])
 
-    for item in b:
-        print(item)
+#    for item in b:
+#        print(item)
 
-#    write_to_db(b)
+    write_to_db(b, ":")
+
+    """
+    So what I currently have won't work, and here's why:
+        If an end item of the same name is found, it's overwritten.
+
+    What if I use the index for the dict key instead of the name....
+    """
+
+
+    data1 = "Expense.Business.Travel.Hotel"
+    data2 = "Expense.Travel.Hotel"
+    adj_list = [(1, "Expense", 1),
+                (2, "Business", 1),
+                (3, "Travel", 2),
+                (4, "Gas", 2),
+                (5, "Hotel", 2),
+                (6, "Airfair", 2),
+                (7, "Travel", 1),
+                (8, "Gas", 7),
+                (9, "Hotel", 7),
+                (10, "Airfair", 7),
+                ]
+    correct1 = (1, 2, 3, 5)
+    correct2 = (1, 7, 9)
